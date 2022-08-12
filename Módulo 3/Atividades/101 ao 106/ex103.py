71874b58746c85690f2ab94ca22070e01d888371
João Castro <joaobomfim7@hotmail.com>
2022-08-12 16:59:35 -0300

    :zap: Resolução do exercicio 103

---
+++ Módulo 3/Atividades/101 ao 106/ex103.py
@@ -0,0 +1,12 @@
+def ficha(nome: str = "<Desconhecido>", gols: int = 0):
+    print('--------------------------------')
+    print(f'O jogador {nome} fez {gols} gol(s) no campeonato.')
+
+
+n = str(input('Nome do Jogador: '))
+g = str(input('Número de Gols: '))
+
+n = "<Desconhecido>" if n.strip() == '' else n
+g = int(g) if g.isnumeric() else 0
+
+ficha(n, g)
